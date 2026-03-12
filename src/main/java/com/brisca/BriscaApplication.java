710373e72421a04dd511d8ef3147d2704f1021f2
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2026-03-12 11:01:37 +0100

    Inicio

---
+++ src/main/java/com/brisca/BriscaApplication.java
@@ -0,0 +1,13 @@
+package com.brisca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriscaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brisca/BriscaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brisca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriscaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
